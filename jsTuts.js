a40baeea1fdc0f63afda8f76a68edb8128070b60
gagangaur <gagan1999gaur@gmail.com>
2019-09-09 23:15:59 +0530

    jsTuts added

---
+++ jsTuts.js
@@ -0,0 +1,70 @@
+// destructuring with string template literals ----------------
+
+let [myname,friend_name] = ['alpha','beta','ceta','theta']
+console.log(`my name is ${myname} and my friend name is ${friend_name}`);
+
+// object literals and object destructuring----------------
+
+function address(city,state){
+  const my_address = {city,state};
+  console.log(my_address);
+}
+address("mathura","uttar pradesh");
+
+function addressmaker(newaddress){
+  const {city,state} = newaddress;
+  const new_address = {
+    city,
+    state,
+    country : 'India'
+  }
+  console.log(new_address);
+}
+addressmaker({city:'mathura',state:'uttar pradesh'});
+
+
+// for of loop -----------------------------------
+
+values = [1,2,3,4,5];
+totalSum = 0;
+for(const val of values){
+  totalSum += val;
+}
+console.log(totalSum);
+
+// spread operator --------------------------
+
+myArray = [1,2,3,4,5,6];
+// newArray = myArray   "this is assignment through reference and change in any one will be reflected in another #shallow copy"
+// depth copy
+newArray = [...myArray] // in this new object is being created and assigned to newArray 
+newArray.push(7);
+console.log(myArray);
+console.log(newArray);
+
+// Rest operator -----------------------
+function showRest(...values){
+//apply ... before the passing argument to pass variable length arguments
+  console.log(values);
+}
+showRest(1,2,3,4,5);
+
+
+
+// CallBack Functions --------------------------------
+
+let add = function(x,y){
+  return x+y;
+};
+
+let multiply = function(x,y){
+  return x*y;
+};
+
+let doWhatever = function(x,y){
+  return `I can do do Whatever i want to do with my numbers ${x} and ${y} `;
+}
+function todo(x,y,CallBack){
+  return CallBack(x,y);
+}
+console.log(todo(2,3,doWhatever));
